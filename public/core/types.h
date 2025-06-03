a43b91b1bb2b287a544df87eb36858dca29fd988
CxRedix <realcxredix@gmail.com>
2025-06-03 12:54:31 +0800

    Isolating Editor From Engine Core

---
+++ public/core/types.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef _FL_CORE_TYPES_H
+#define _FL_CORE_TYPES_H
+
+typedef unsigned char b8;
+typedef unsigned int  u32;
+typedef unsigned long u64;
+typedef char* c_str;
+typedef const char* literal_str;
+
+
+#endif // _FL_CORE_TYPES_H
--- public/utils.h
+++ public/utils.h
@@ -2,14 +2,10 @@
 #ifndef _UTILS_H
 #define _UTILS_H
 
+#include <core/types.h>
+
 #include <cglm/types.h>
 #include <GLFW/glfw3.h>
-
-typedef unsigned char b8;
-typedef unsigned int  u32;
-typedef unsigned long u64;
-typedef char* c_str;
-typedef const char* literal_str;
 
 #define arr_size(ARR) (sizeof(ARR) / sizeof(ARR[0]))
 
